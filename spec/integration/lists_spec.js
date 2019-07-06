32faa08a452b5d4511f5ad5cbb6af95d429b2b28
Richard Tinajero <rj.tinajero92@gmail.com>
2019-07-06 15:33:27 -0700

    setup update

--- spec/integration/lists_spec.js
+++ spec/integration/lists_spec.js
@@ -1,10 +1,12 @@
 const request = require("request");
 const server = require("../../src/server");
-const base = "http://localhost:3000/";
+const base = "http://localhost:3000/topics/";
 
-describe("routes : static", () => {
-  describe("GET /", () => {
-    it("should return status code 200", (done) => {
+describe("routes : topics", () => {
+
+  describe("GET /topics", () => {
+
+    it("should return a status code 200", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         done();
