ce251e3390f5fd4aa9c04b05d433edaf0625c86b
Richard Tinajero <rj.tinajero92@gmail.com>
2019-07-06 15:51:29 -0700

    setup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 
-
 const routeConfig = require("./config/route-config.js");
+const appConfig = require("./config/main-config");
 
+appConfig.init();
 routeConfig.init(app);
 
 module.exports = app;
