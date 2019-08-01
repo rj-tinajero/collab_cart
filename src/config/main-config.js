1e299a59e94c88ce383b666ca5a64387b9d0191b
Richard Tinajero <rj.tinajero92@gmail.com>
2019-08-01 09:14:43 -0700

    build edits

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -27,6 +27,13 @@ module.exports = {
       saveUninitialized: false,
       cookie: { maxAge: 1.21e+9 }
     }));
+    if(process.env.NODE_ENV === 'production') {
+      app.use(express.static('../../client/build'));
+      const path = require('path');
+      app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      })
+    }
     app.use(flash());
     // passportConfig.init(app);
 
--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ const app = require("./app");
 const http = require("http");
 
 const port = normalizePort(process.env.PORT || "5000");
+
+
+
 app.set("port", port);
 
 const server = http.createServer(app);
