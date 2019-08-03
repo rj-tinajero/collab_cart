adc5e54216c4da80b8acaa56514418e9e2754bd2
Richard Tinajero <rj.tinajero92@gmail.com>
2019-08-02 22:27:40 -0700

    update deploy again

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -30,9 +30,10 @@ module.exports = {
     if(process.env.NODE_ENV === 'production') {
       app.use(express.static('client/build'));
       
-      app.get('*', (req, res) => {
+      app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
       })
+
     }
     app.use(flash());
     // passportConfig.init(app);
