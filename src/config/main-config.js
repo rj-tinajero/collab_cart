af4d5bf43b4c8a60d6e29b8406bcbad139a8f006
Richard Tinajero <rj.tinajero92@gmail.com>
2019-08-02 22:08:33 -0700

    update deploy

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -29,7 +29,7 @@ module.exports = {
     }));
     if(process.env.NODE_ENV === 'production') {
       app.use(express.static('client/build'));
-      const path = require('path');
+      
       app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
       })
