96d928d486702beaaf6a94c189709b22d092caaf
Richard Tinajero <rj.tinajero92@gmail.com>
2019-08-03 09:12:35 -0700

    update file paths

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -32,7 +32,10 @@ module.exports = {
       
       app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-      })
+      });
+      app.get('/auth/google/callback', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      });
 
     }
     app.use(flash());
