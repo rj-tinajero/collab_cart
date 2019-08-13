e763fe05d3a4b3f182cc154168a0e2082d40bc35
Richard Tinajero <rj.tinajero92@gmail.com>
2019-08-13 11:47:59 -0700

    passport edit

--- src/config/passport.js
+++ src/config/passport.js
@@ -9,7 +9,7 @@ passport.deserializeUser(function(user, done) {
  done(null, user);
 });
 
-if(process.env.NODE_ENV === 'production') {
+
   passport.use(
     new GoogleStrategy(
      {
@@ -45,41 +45,41 @@ if(process.env.NODE_ENV === 'production') {
      },
     )
    );
-}
 
-passport.use(
- new GoogleStrategy(
-  {
-   clientID: process.env.CLIENT_ID,
-   clientSecret: process.env.CLIENT_SECRET,
-   callbackURL: "http://localhost:5000/auth/google/callback",
-   proxy: true
-  },
-  function(accessToken, refreshToken, profile, done) {
-     console.log('access token', accessToken); 
-     console.log('refresh token', refreshToken);
-     console.log('profile', profile.emails[0].value);
-   var userData = {
-    email: profile.emails[0].value,
-    name: profile.displayName,
-    token: accessToken
-   };
-   User.findOrCreate({ where: {email: userData.email} }).then(
-     user => {
-       if(!user) { 
-        return User.create({ 
-           email: userData.email
-         })
-       } else { 
-         return Promise.resolve(user);
-       }
-     }
-   )
+
+// passport.use(
+//  new GoogleStrategy(
+//   {
+//    clientID: process.env.CLIENT_ID,
+//    clientSecret: process.env.CLIENT_SECRET,
+//    callbackURL: "http://localhost:5000/auth/google/callback",
+//    proxy: true
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+//      console.log('access token', accessToken); 
+//      console.log('refresh token', refreshToken);
+//      console.log('profile', profile.emails[0].value);
+//    var userData = {
+//     email: profile.emails[0].value,
+//     name: profile.displayName,
+//     token: accessToken
+//    };
+//    User.findOrCreate({ where: {email: userData.email} }).then(
+//      user => {
+//        if(!user) { 
+//         return User.create({ 
+//            email: userData.email
+//          })
+//        } else { 
+//          return Promise.resolve(user);
+//        }
+//      }
+//    )
     
-    done(null, userData);
+//     done(null, userData);
   
-  //  console.log(this.user, "KJBIUBWSDPIUWBCP");
-  //  done(null, userData);
-  },
- )
-);
+//   //  console.log(this.user, "KJBIUBWSDPIUWBCP");
+//   //  done(null, userData);
+//   },
+//  )
+// );
