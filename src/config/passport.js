fdd803737a163c34031cbbbb3f0ec20f2763a648
Richard Tinajero <rj.tinajero92@gmail.com>
2019-08-16 12:42:30 -0700

    env update

--- src/config/passport.js
+++ src/config/passport.js
@@ -10,64 +10,64 @@ passport.deserializeUser(function(user, done) {
 });
 
 
-  // passport.use(
-  //   new GoogleStrategy(
-  //    {
-  //     clientID: process.env.CLIENT_ID,
-  //     clientSecret: process.env.CLIENT_SECRET,
-  //     callbackURL: "https://rj-tinajero-collabcart.herokuapp.com/auth/google/callback",
-  //    },
-  //    function(accessToken, refreshToken, profile, done) {
-  //     var userData = {
-  //      email: profile.emails[0].value,
-  //      name: profile.displayName,
-  //      token: accessToken
-  //     };
-  //     User.findOrCreate({ where: {email: userData.email} }).then(
-  //       user => {
-  //         if(!user) { 
-  //          return User.create({ 
-  //             email: userData.email
-  //           })
-  //         } else { 
-  //           return Promise.resolve(user);
-  //         }
-  //       }
-  //     )
+  passport.use(
+    new GoogleStrategy(
+     {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "https://rj-tinajero-collabcart.herokuapp.com/auth/google/callback",
+     },
+     function(accessToken, refreshToken, profile, done) {
+      var userData = {
+       email: profile.emails[0].value,
+       name: profile.displayName,
+       token: accessToken
+      };
+      User.findOrCreate({ where: {email: userData.email} }).then(
+        user => {
+          if(!user) { 
+           return User.create({ 
+              email: userData.email
+            })
+          } else { 
+            return Promise.resolve(user);
+          }
+        }
+      )
        
-  //      done(null, userData);
-  //    },
-  //   )
-  //  );
-
-
-passport.use(
- new GoogleStrategy(
-  {
-   clientID: process.env.CLIENT_ID,
-   clientSecret: process.env.CLIENT_SECRET,
-   callbackURL: "http://localhost:5000/auth/google/callback",
-   proxy: true
-  },
-  function(accessToken, refreshToken, profile, done) {
-    var userData = {
-      email: profile.emails[0].value,
-      name: profile.displayName,
-      token: accessToken
-     };
-   User.findOrCreate({ where: {email: userData.email} }).then(
-     user => {
-       if(!user) { 
-        return User.create({ 
-           email: userData.email
-         })
-       } else { 
-         return Promise.resolve(user);
-       }
-     }
+       done(null, userData);
+     },
+    )
    );
 
-    done(null, userData);
-  },
- )
-);
+
+// passport.use(
+//  new GoogleStrategy(
+//   {
+//    clientID: process.env.CLIENT_ID,
+//    clientSecret: process.env.CLIENT_SECRET,
+//    callbackURL: "http://localhost:5000/auth/google/callback",
+//    proxy: true
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+//     var userData = {
+//       email: profile.emails[0].value,
+//       name: profile.displayName,
+//       token: accessToken
+//      };
+//    User.findOrCreate({ where: {email: userData.email} }).then(
+//      user => {
+//        if(!user) { 
+//         return User.create({ 
+//            email: userData.email
+//          })
+//        } else { 
+//          return Promise.resolve(user);
+//        }
+//      }
+//    );
+
+//     done(null, userData);
+//   },
+//  )
+// );
